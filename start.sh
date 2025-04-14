b5c0cf4c23eb5ec53fe48f77bcab8dc17b9085e8
go2inidia <64279625+go2inidia@users.noreply.github.com>
2025-04-14 14:37:04 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 COOKIES="/tmp/cookies.txt"
+CURRENT_PORT=""
 
 update_port () {
-  PORT=$(cat $PORT_FORWARDED)
-  rm -f $COOKIES
-  curl -s -c $COOKIES --data "username=$QBITTORRENT_USER&password=$QBITTORRENT_PASS" ${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/auth/login > /dev/null
-  curl -s -b $COOKIES --data 'json={"listen_port": "'"$PORT"'"}' ${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/app/setPreferences > /dev/null
-  rm -f $COOKIES
-  echo "Successfully updated qbittorrent to port $PORT"
+  NEW_PORT=$(cat $PORT_FORWARDED)
+  if [ "$NEW_PORT" != "$CURRENT_PORT" ]; then
+    CURRENT_PORT=$NEW_PORT
+    rm -f $COOKIES
+    curl -s -c $COOKIES --data "username=$QBITTORRENT_USER&password=$QBITTORRENT_PASS" ${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/auth/login > /dev/null
+    curl -s -b $COOKIES --data 'json={"listen_port": "'"$CURRENT_PORT"'"}' ${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/app/setPreferences > /dev/null
+    rm -f $COOKIES
+    echo "Successfully updated qbittorrent to port $CURRENT_PORT"
+  fi
 }
 
 while true; do
   if [ -f $PORT_FORWARDED ]; then
     update_port
-     inotifywait -mq -t ${PORT_UPDATE_TIMEOUT:-0} -e close_write $PORT_FORWARDED | while read change; do
+    inotifywait -mq -t ${PORT_UPDATE_TIMEOUT:-0} -e close_write $PORT_FORWARDED | while read change; do
       update_port
     done
   else
